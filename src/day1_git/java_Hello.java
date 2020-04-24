49f4704a53a7502fdf7d8df2c58c1b1dc30d9838
izzat12 <Izzatpatidinov@gmail.com>
2020-04-24 15:35:01 -0400

    first commit

---
+++ src/day1_git/java_Hello.java
@@ -0,0 +1,7 @@
+package day1_git;
+
+public class java_Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ src/pratice/gitHub.java
@@ -0,0 +1,7 @@
+package pratice;
+
+public class gitHub {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
